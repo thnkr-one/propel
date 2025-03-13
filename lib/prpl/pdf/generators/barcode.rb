b27b25721d1c1bbf19e501454bdc52dbc08db9e0
J M <macadelic@Js-Mac-mini.local>
2025-03-12 20:25:30 -0400

    added types to barcode.rb/.rbs

--- lib/prpl/pdf/generators/barcode.rb
+++ lib/prpl/pdf/generators/barcode.rb
@@ -29,118 +29,118 @@ module Prpl
 
         protected
 
-          def generate_pdf
-            Prpl.logger.info "Generating barcode PDF", context: barcode_context
+        def generate_pdf
+          Prpl.logger.info "Generating barcode PDF", context: barcode_context
 
-            document = create_document(
-              page_size: options[:page_size] || :letter,
-              margin: calculate_margins
-            )
+          document = create_document(
+            page_size: options[:page_size] || :letter,
+            margin: calculate_margins
+          )
 
-            if barcode_content.present?
-              add_barcode(document)
-            else
-              document.text "No Barcode Available",
-                            align: :center,
-                            valign: :center
-            end
-
-            add_details(document)
-            document.render
+          if barcode_content.present?
+            add_barcode(document)
+          else
+            document.text "No Barcode Available",
+                          align: :center,
+                          valign: :center
           end
 
-          def generate_filename
-            "barcode_#{variant_identifier}.pdf"
-          end
+          add_details(document)
+          document.render
+        end
+
+        def generate_filename
+          "barcode_#{variant_identifier}.pdf"
+        end
 
         private
 
-          def add_barcode(document)
-            barcode = Barby::Code128B.new(barcode_content)
+        def add_barcode(document)
+          barcode = Barby::Code128B.new(barcode_content)
 
-            temp_file_path = generate_barcode_image(barcode)
+          temp_file_path = generate_barcode_image(barcode)
 
-            if temp_file_path && File.exist?(temp_file_path)
-              document.image temp_file_path,
-                             at: [document.bounds.left, document.cursor],
-                             width: BARCODE_DEFAULTS[:width]
-            end
+          if temp_file_path && File.exist?(temp_file_path)
+            document.image temp_file_path,
+                           at: [document.bounds.left, document.cursor],
+                           width: BARCODE_DEFAULTS[:width]
+          end
+        end
+
+        def generate_barcode_image(barcode)
+          temp_file = create_temp_file(prefix: 'barcode', suffix: '.png')
+          temp_file.binmode
+
+          barcode_image = barcode.to_png(
+            height: @barcode_options[:height],
+            xdim: @barcode_options[:xdim],
+            margin: @barcode_options[:margin]
+          )
+
+          temp_file.write(barcode_image)
+          temp_file.rewind
+          temp_file.path
+        end
+
+        def add_details(document)
+          document.move_down 10
+
+          if variant_sku
+            document.text "SKU: #{variant_sku}",
+                          align: :center,
+                          size: BARCODE_DEFAULTS[:text_size][:sku]
           end
 
-          def generate_barcode_image(barcode)
-            temp_file = create_temp_file(prefix: 'barcode', suffix: '.png')
-            temp_file.binmode
-
-            barcode_image = barcode.to_png(
-              height: @barcode_options[:height],
-              xdim: @barcode_options[:xdim],
-              margin: @barcode_options[:margin]
-            )
-
-            temp_file.write(barcode_image)
-            temp_file.rewind
-            temp_file.path
+          if variant_price
+            document.text format_currency(variant_price),
+                          align: :center,
+                          size: BARCODE_DEFAULTS[:text_size][:price]
           end
+        end
 
-          def add_details(document)
-            document.move_down 10
-
-            if variant_sku
-              document.text "SKU: #{variant_sku}",
-                            align: :center,
-                            size: BARCODE_DEFAULTS[:text_size][:sku]
-            end
-
-            if variant_price
-              document.text format_currency(variant_price),
-                            align: :center,
-                            size: BARCODE_DEFAULTS[:text_size][:price]
-            end
+        def calculate_margins
+          if options[:margins]
+            process_margins(options[:margins])
+          else
+            process_margins(layout_options)
           end
+        end
 
-          def calculate_margins
-            if options[:margins]
-              process_margins(options[:margins])
-            else
-              process_margins(layout_options)
-            end
+        def validate_barcode_data!
+          unless variant_identifier && (barcode_content || qr_code_data)
+            raise Prpl::Errors::Error, "Variant identifier and either barcode content or QR code data must be provided"
           end
+        end
 
-          def validate_barcode_data!
-            unless variant_identifier && (barcode_content || qr_code_data)
-              raise Prpl::Errors::Error, "Variant identifier and either barcode content or QR code data must be provided"
-            end
-          end
+        def barcode_content
+          options[:barcode_content]
+        end
 
-          def barcode_content
-            options[:barcode_content]
-          end
+        def variant_identifier
+          options[:variant_identifier]
+        end
 
-          def variant_identifier
-            options[:variant_identifier]
-          end
+        def variant_sku
+          options[:variant_sku]
+        end
 
-          def variant_sku
-            options[:variant_sku]
-          end
+        def variant_price
+          options[:variant_price]
+        end
 
-          def variant_price
-            options[:variant_price]
-          end
+        def qr_code_data
+          options[:qr_code_data]
+        end
 
-          def qr_code_data
-            options[:qr_code_data]
-          end
-
-          def barcode_context
-            {
-              variant_identifier: variant_identifier,
-              variant_sku: variant_sku,
-              has_barcode: !barcode_content.nil?,
-              has_qr: !qr_code_data.nil?,
-              barcode_options: @barcode_options
-            }
-          end
+        def barcode_context
+          {
+            variant_identifier: variant_identifier,
+            variant_sku: variant_sku,
+            has_barcode: !barcode_content.nil?,
+            has_qr: !qr_code_data.nil?,
+            barcode_options: @barcode_options
+          }
+        end
       end
 
       # Specialized version for Barcode QR combinations
@@ -152,77 +152,77 @@ module Prpl
 
         protected
 
-          def generate_pdf
-            Prpl.logger.info "Generating barcode QR PDF", context: barcode_context
+        def generate_pdf
+          Prpl.logger.info "Generating barcode QR PDF", context: barcode_context
 
-            document = create_document(
-              page_size: [POINTS_PER_INCH, POINTS_PER_INCH],
-              margin: [2, 2, 2, 2]  # 2 point margins for roll printing
-            )
+          document = create_document(
+            page_size: [POINTS_PER_INCH, POINTS_PER_INCH],
+            margin: [2, 2, 2, 2]  # 2 point margins for roll printing
+          )
 
-            if qr_code_data
-              add_qr_code(document)
-              add_price_to_page(document)
-            else
-              document.text "No QR Code Available",
-                            align: :center,
-                            size: 16
-            end
-
-            document.render
+          if qr_code_data
+            add_qr_code(document)
+            add_price_to_page(document)
+          else
+            document.text "No QR Code Available",
+                          align: :center,
+                          size: 16
           end
 
-          def generate_filename
-            "barcode_qr_#{variant_identifier}.pdf"
-          end
+          document.render
+        end
+
+        def generate_filename
+          "barcode_qr_#{variant_identifier}.pdf"
+        end
 
         private
 
-          def add_qr_code(document)
-            dimensions = calculate_dimensions(document)
-            temp_file_path = write_image_to_temp_file(qr_code_data)
+        def add_qr_code(document)
+          dimensions = calculate_dimensions(document)
+          temp_file_path = write_image_to_temp_file(qr_code_data)
 
-            if File.exist?(temp_file_path)
-              document.image temp_file_path,
-                             at: [
-                                   dimensions[:x_position],
-                                   dimensions[:y_position] + dimensions[:qr_size]
-                                 ],
-                             width: dimensions[:qr_size]
-            end
+          if File.exist?(temp_file_path)
+            document.image temp_file_path,
+                           at: [
+                                 dimensions[:x_position],
+                                 dimensions[:y_position] + dimensions[:qr_size]
+                               ],
+                           width: dimensions[:qr_size]
           end
+        end
 
-          def add_price_to_page(document)
-            dimensions = calculate_dimensions(document)
+        def add_price_to_page(document)
+          dimensions = calculate_dimensions(document)
 
-            document.font_size 8 do
-              document.text_box format_currency(variant_price),
-                                at: [0, 12],
-                                width: dimensions[:usable_width],
-                                height: 10,
-                                align: :center
-            end
+          document.font_size 8 do
+            document.text_box format_currency(variant_price),
+                              at: [0, 12],
+                              width: dimensions[:usable_width],
+                              height: 10,
+                              align: :center
           end
+        end
 
-          def calculate_dimensions(document)
-            usable_width = document.bounds.width
-            usable_height = document.bounds.height
-            qr_size = [usable_width, usable_height * 0.8].min
+        def calculate_dimensions(document)
+          usable_width = document.bounds.width
+          usable_height = document.bounds.height
+          qr_size = [usable_width, usable_height * 0.8].min
 
-            {
-              usable_width: usable_width,
-              usable_height: usable_height,
-              qr_size: qr_size,
-              x_position: (usable_width - qr_size) / 2,
-              y_position: usable_height - 2 - qr_size
-            }
+          {
+            usable_width: usable_width,
+            usable_height: usable_height,
+            qr_size: qr_size,
+            x_position: (usable_width - qr_size) / 2,
+            y_position: usable_height - 2 - qr_size
+          }
+        end
+
+        def validate_qr_data!
+          unless qr_code_data
+            raise Prpl::Errors::Error, "QR code data must be provided for BarcodeQr generation"
           end
-
-          def validate_qr_data!
-            unless qr_code_data
-              raise Prpl::Errors::Error, "QR code data must be provided for BarcodeQr generation"
-            end
-          end
+        end
       end
     end
   end
